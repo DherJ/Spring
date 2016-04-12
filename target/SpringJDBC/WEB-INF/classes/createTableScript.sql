1559a78e8073ed7a8a609e7f0b8bb00cbecfcf79
DherJ <j.dhersin@hotmail.fr>
2016-04-12 11:41:22 +0200

    commit SpringJDBC

---
+++ target/SpringJDBC/WEB-INF/classes/createTableScript.sql
@@ -0,0 +1,7 @@
+CREATE TABLE usertable (
+	id VARCHAR(11) NOT NULL,
+	name VARCHAR(11) NOT NULL,
+	firstName VARCHAR(11) NOT NULL,
+	email VARCHAR(11) NOT NULL,
+	age integer(11) NOT NULL,
+	PRIMARY KEY (id));
