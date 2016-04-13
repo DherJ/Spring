6ec0506bfd8be2a9e1e5c7dd0b8e5533c10c106a
DherJ <j.dhersin@hotmail.fr>
2016-04-13 11:42:16 +0200

    commit

---
+++ src/main/java/com/tutorial/MainApp.java
@@ -0,0 +1,10 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConfiguration.class);
+	}
+}
